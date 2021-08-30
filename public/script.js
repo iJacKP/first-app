bf52224bbcfa1ad1fa9d240e6934fe6c3d4a13bb
Jack <jackson_d2@outlook.com>
2021-08-30 20:54:00 -0300

    last commit
    Dando toques finais no First App

--- public/script.js
+++ public/script.js
@@ -1,81 +1,61 @@
 let cadastro;
 
-//SEMPRE LEMBRAR requisição -> middleware -> resposta
+function update(index,link){ 
 
-function update(index,link){ //link aqui é /cadastro/update/
-    //seleciona todas as tags que sejam td 
-    let tds = document.querySelectorAll(`td[data-index-row='${index}']`); //as td's contêm os itens desejados, será útil selecioná-las
-    let spans = document.querySelectorAll(`td[data-index-row='${index}'] > span`); //dentro da td especificada, selecionamos os span dentro dela (>span)
-    let inputs = document.querySelectorAll(`td[data-index-row='${index}'] > input`); //dentro da td especificada, selecionamos os input dentro dela (>input)
+    let tds = document.querySelectorAll(`td[data-index-row='${index}']`); 
+    let spans = document.querySelectorAll(`td[data-index-row='${index}'] > span`);
+    let inputs = document.querySelectorAll(`td[data-index-row='${index}'] > input`); 
 
-    let lenTds = tds.length-1; //numero de tds em uma linha da tabela (especificamente, da linha que chamou a função)
-    let linkUpdate = tds[lenTds-1]; //se refere à td que contém o icone de update(lápis)
-    let linkRemove = tds[lenTds]; // se refere à td que contém o ícone de delete(trash)
+    let lenTds = tds.length-1; 
+    let linkUpdate = tds[lenTds-1]; 
+    let linkRemove = tds[lenTds];
 
-    let lenInputs = inputs.length; //conta quantos inputs existem na tabela(visíveis ou não, estão lá)
+    let lenInputs = inputs.length;
 
-    let button = inputs[lenInputs-1]; //o ultimo input da lista(sinalizado pelo tamanho -1 no vetor)possui o botão de 'atualizar', que deve ser selecinado e mostrado depois
+    let button = inputs[lenInputs-1];
 
 
 
-    linkUpdate.className='hidden'; //com o icone de update selecionado, temos que fazê-lo sumir (e depois fazer os inputs com atributo 'hidden' aparecer)
-    linkRemove.className='hidden'; //mesma coisa, selecionamos o icone ali em cima e setamos a classe 'hidden' pra ele
-    tds[lenTds-2].className='show'; //mostra a td que contém o botão de 'atualizar'
-
-     //esconde todos os campos de exibição de dados do cadastro
-    for(let cont=0;cont<spans.length;cont++){ //busca pela seguinte coisa: se um span está visível, torne-o invisível
-        if(spans[cont].className=="show"){    //senão, deixe-o visível
+    linkUpdate.className='hidden'; 
+    linkRemove.className='hidden'; 
+    tds[lenTds-2].className='show'; 
+    
+    for(let cont=0;cont<spans.length;cont++){ 
+        if(spans[cont].className=="show"){   
             spans[cont].className="hidden";
         } else{
             spans[cont].className="show";
         }
     }
-    //mostra os campos de preenchimento para o cadastro
-    for(let cont=0;cont<inputs.length;cont++){ //mesma coisa do de cima, só que para os inputs que colocaremos a informação
+
+    for(let cont=0;cont<inputs.length;cont++){ 
         if(inputs[cont].className=="hidden"){
             inputs[cont].className="show";
         }
     }
 
-    //escuta se o botao foi clicado
+   
     button.addEventListener('click',()=>{
-        const http = new XMLHttpRequest(); //XML - cria um objeto para requisição ao servidor
-        const url=link; // "/cadastro/update";
+        const http = new XMLHttpRequest(); 
+        const url=link; 
         let data = {id:"",name:"",email:"",address:"",age:"",heigth:"",vote:""};
         let dataToSend;
 
 
 
-        http.open("POST",link,true); //abre uma comunicação com o servidor através de uma requisição POST
-        //Se no servidor nao houver um elemento esperando por uma mensagem POST (ex. router.post()) para a rota /cadastro/update ocorrerar um erro: 404 - File Not Found
+        http.open("POST",link,true); 
+        
 
-        //Dados HTML teria no cabecalho HEADER (da mensagem HTTP) - Content-Type= text/html
-        //Dados estruturados como querystring (ex: http//www.meu.com.br:3030/?campo=meu&campo2=10) -  Content-Type=x-www-form-urlencoded
-        //Dados no formato de Objeto Javascript para troca de informacoes (JSON) Content-Type=application/json : Ex.: {key1:value1,key2:value2}
-        http.setRequestHeader('Content-Type','application/json'); //constroi um cabecalho http para envio dos dados
+        
+        http.setRequestHeader('Content-Type','application/json'); 
          
-        for(let cont=0;cont<inputs.length;cont++){ //desabilita todos os inputs para escrita ou acesso (no caso do button)
+        for(let cont=0;cont<inputs.length;cont++){ 
             if(inputs[cont].disabled==true){
                 inputs[cont].disabled=false;
             } else inputs[cont].disabled=true;
         }
-    //    // essa suncao esta sendo colocada aqui só para dar uma parada e você poder ver os inputs desabilitados
-    //    //funcao que espera um tempo N, dado em milissegundos, e então chama uma função (callback). No caso, vamos usar 2000 ms = 2s
-    //    //essa funcao foi construida somente para que voce possa ver os inputs ficando desabilitados. Nao precisa usar.
-    //    function sleep(milliseconds) {
-    //         const date = Date.now();
-    //         let currentDate = null;
-    //         do {
-    //             currentDate = Date.now();
-    //         } while (currentDate - date < milliseconds);
-    //     }
-    //     console.log("Mostra essa mensagem no console, primeiro!");
-    //     sleep(2000)
-    //     console.log("Pronto, você consegue ver seus inputs desabilitados!");
-    //    //fim do codigo usado para ver os inputs desabiulitados
-
-        //preenche um objeto com o indice da linha da tabela e os valores dos campos input do tipo text
-        data.id = index; //esse dado nao existe no vetor Users do lado do servidor (backend), mas preciso dele para apontar o indice do vetor que quero modificar
+  
+        data.id = index;
         data.name = inputs[0].value;
         data.email = inputs[1].value;
         data.address = inputs[2].value;
@@ -83,28 +63,16 @@ function update(index,link){ //link aqui é /cadastro/update/
         data.heigth = inputs[4].value;
         data.vote = inputs[5].value;
 
-        dataToSend = JSON.stringify(data); //transforma o objeto literal em uma string JSON que é a representação em string de um objeto JSON. Se quisesse o objeto no formato binario, usaria: JSON.parse(data)
+        dataToSend = JSON.stringify(data); 
 
-        http.send(dataToSend);//envia dados para o servidor na forma de JSO
+        http.send(dataToSend);
 
-        /* este codigo abaixo foi colocado para que a interface de cadastro so seja modificada quando se receber um aviso do servidor que a modificacao foi feita com sucesso. No caso o aviso vem na forma do codigo 200 de HTTP: OK */
+        
         http.onload = ()=>{ 
 
-            /*
-            readyState:
-            0: request not initialized
-            1: server connection established
-            2: request received
-            3: processing request
-            4: request finished and response is ready
-            status:
-            200: "OK"
-            403: "Forbidden"
-            404: "Page not found"
-            */
-            // baseado nos valores acima apresentados, o codigo abaixo mostra o que foi enviado pelo servidor como resposta ao envio de dados. No caso, se o request foi finalizado e o response foi recebido, a mensagem recebida do servidor eh mostrada no console do navegador. esse codigo foi feito apenas para verificar se tudo ocorreu bem no envio
+          
 
-            if (http.readyState === 4 && http.status === 200) { //testa se o envio foi bem sucedido
+            if (http.readyState === 4 && http.status === 200) { 
                 for(let cont=0;cont<spans.length;cont++){
                     if(spans[cont].className=="hidden"){
                         spans[cont].innerHTML = inputs[cont].value;
@@ -114,11 +82,11 @@ function update(index,link){ //link aqui é /cadastro/update/
                     }
                 }
 
-                //esconde os campos de preenchimento para o cadastro
+               
                 for(let cont=0;cont<inputs.length;cont++){
                     if(inputs[cont].className=="show"){
                         inputs[cont].className="hidden";
-                        if(inputs[cont].disabled==false){//habilita novamente os inputs para escrita
+                        if(inputs[cont].disabled==false){
                             inputs[cont].disabled=true;
                         }
                     }
@@ -154,7 +122,7 @@ function remove(index,_name,link){
     dataToSend = JSON.stringify({name:_name}); 
     
 
-    http.send(dataToSend);//envia dados para o servidor na forma de JSON
+    http.send(dataToSend);
    
 
     http.onload = ()=>{ 
--- routes.js
+++ routes.js
@@ -5,21 +5,9 @@ const router = express.Router();
 
 
 router.use(express.static('public'));
-//especifica a pasta contendo arquivos estáticos.
-//o nome 'public' não precisará ser colocado no inicio da rota que se refira a eles
-// por padrão, se não houver conteúdo na rota para ser usado, vai ser o da pasta com conteudo estático padrão: public
 
-//Exemplo de rotas:
-/*
-http://localhost:3000/css
-http://localhost:3000/images
-http://localhost:3000/index.html
-*/
-
-//método get: quando o usuário solicita conteúdo daquela rota ('/' nesse caso) a funcionalidade é executada
-//os parametros na callback(req,res) são passados pelo método get: a requisição de info feita ao navegador e a resposta para ele. os nomes das var. não importam.
 router.get('/', (req, res)=>{ 
-    res.render('pages/home') //posso omitir a extensão (era pra ser home.ejs)
+    res.render('pages/home') 
 })
 
 router.get('/about', (req,res)=>{
@@ -27,32 +15,31 @@ router.get('/about', (req,res)=>{
 });
 
 router.get('/cadastro',(req,res)=>{
-    //a funcao render pode receber um pametro na forma de objeto literal
-    //no caso, ela irá receber um objeto com campo chamado users e com valor igual ao vetor users
+   
     res.render('pages/cadastro', {users: users})
 });
 
 router.post('/cadastro/remove', (req,res)=>{
-    let name = req.body.name; //exportamos, lá em script, a informação em forma de JSON para essa rota. dessa forma podemos acessar aqui a informação enviada de lá (estava na forma {name: nomeVariável})
+    let name = req.body.name; 
     console.log(req.body)
-    if(users.length==0){ //aqui testamos para o caso do vetor users não possuir usuários no momento:
+    if(users.length==0){ 
         console.log("Erro: Não há elemento a ser removido!")
         return res.status(500).json({
             status:'error',
             error:`Removed element: ${name}`
         });
 
-    } else { //caso ele tenha usuários vamos manipulá-lo
+    } else { 
         for(let cont=0;cont<users.length;cont++){
-            if(users[cont].name==name){ //procuramos (com o for) no vetor de usuários da lista se algum deles bate com o usuário que queremos excluir
-                users.splice(cont,1); //achamos o usuário desejado-> vamos remover todos os itens relacionados a esse usuário (o objeto literal que corresponde a ele em users)
-                console.log("Elemento Removido: ",name) //loga no console que deu certo e diz qual usuário foi deletado
-                return res.status(200).json({ //outro ponto importante: aqui dizemos para o servidor que a operação foi concluída, mandando o status e um outro arquivo JSON(linguagem do servidor)
-                    status:'sucess',            //indicando sucesso e contendo o novo array de usuários, agora atualizado(sem o usuário deletado)
+            if(users[cont].name==name){ 
+                users.splice(cont,1); 
+                console.log("Elemento Removido: ",name) 
+                return res.status(200).json({ 
+                    status:'sucess',          
                     data:users
                 });
-                //res.send(JSON.stringify({sucess:`Elemento removido com sucesso: ${name}`}));
-            } else if(cont==users.length-1){ //se o for percorrer toda a lista e não achar, relatamos um erro(a mesma lógica acima)
+              
+            } else if(cont==users.length-1){ 
                 console.log("Erro ao remover elemento: ",name);
                 return res.status(400).json({
                     status:'error',
@@ -64,10 +51,9 @@ router.post('/cadastro/remove', (req,res)=>{
 });
 
 router.post('/cadastro/update', (req,res)=>{
-    //essa função substitui os valores armazenados no indice 'id' do vetor users por valores recebidos do navegador (armazenados em req.body)
-    //recebe dados do cliente/navegador na forma de um objeto JSON (possui diversas propriedades (.name, .email, por exemplo) que podem ser acessadas diretamente)
+   
 
-    users[req.body.id].name=req.body.name; //req.body: objeto contendo toda a informação enviada | .name: uma das propriedades do 'objetão'
+    users[req.body.id].name=req.body.name; 
     users[req.body.id].email=req.body.email;
     users[req.body.id].address=req.body.address;
     users[req.body.id].age=req.body.age;
@@ -75,15 +61,15 @@ router.post('/cadastro/update', (req,res)=>{
     users[req.body.id].vote=req.body.vote;
 
 
-    res.sendStatus(200); //envia mensagem 200 significando que as modificacoes deram certo
-    console.log("Dados recebidos: ",req.body);//mostra no console do servidor os dados recebidos
+    res.sendStatus(200); 
+    console.log("Dados recebidos: ",req.body);
 })
 
 router.get('/listagem',(req,res)=>{
-    // console.log("Olha a lista ae: ",users)
+   
     console.log('listagem funcionando(RENDERIZANDO /LISTAGEM)')
     res.status(200).render('pages/listagem')
-    // res.status(200).json({users: users})
+    
     
 });
 router.get('/listagem/update',(req,res)=>{
@@ -109,17 +95,11 @@ router.post('/cadastro/add',(req,res)=>{
     user.age = req.body.age
     user.vote = req.body.vote
 
-    // user.name = req.body._name;
-    // user.email = req.body._email;
-    // user.address = req.body._address;
-    // user.heigth = req.body._heigth;
-    // user.age = req.body._age;
-    // user.vote = req.body._vote;
+
 
     users.push(user);
     console.log("Usuário cadastrado(ROTA POST): ",user);
-    // // console.log("Lista dos usuários: ",users); //nao use esta linha se tiver muitos elementos em users pois causara lentidao no servidor
-    // res.sendStatus(200);
+  
     res.status(200).json({
         status:'sucess',
         data: users
